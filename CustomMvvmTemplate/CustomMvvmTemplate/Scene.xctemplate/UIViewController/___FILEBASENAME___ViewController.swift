06338fb1d700718c5dc2606cb40c4ffbc81e8ac8
Merve Şahan <merve.sahan@ibtech.com.tr>
2019-11-15 10:02:19 +0300

    modify

---
+++ CustomMvvmTemplate/CustomMvvmTemplate/Scene.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+class ___VARIABLE_sceneName___ViewController: BaseViewController<___VARIABLE_sceneName___ViewModel, ___VARIABLE_sceneName___Model>
+{
+    // Connect IBOutlet
+    // @IBOutlet weak var txtAccountName: UITextField!
+    
+    // Initialize variable :
+    //var accountName: String
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        bindViewModel()
+        viewModel.fetchData()
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func bindViewModel() {
+        
+    }
+    
+    
+    // Connect IBOutlet
+    //@IBAction func btnclick(_ sender: Any) {
+    //
+    //}
+    
+}
