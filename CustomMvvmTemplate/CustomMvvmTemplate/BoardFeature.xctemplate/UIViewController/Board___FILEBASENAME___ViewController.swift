7c6dbd16a98ec492d2e60ff61629ec4451a5d7bc
Mine Oz <mineoz621@gmail.com>
2020-03-25 11:06:37 +0300

    Add Board Template

---
+++ CustomMvvmTemplate/CustomMvvmTemplate/BoardFeature.xctemplate/UIViewController/Board___FILEBASENAME___ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ IBTECH. All rights reserved.
+//
+
+// MARK: Import (in alphabetical order)
+import MobileCore
+import RxCocoa
+import RxSwift
+import UIKit
+
+/**
+- When you use `BoardPartViewController`, you must add two `ControlViews` to your .xib file with `ContainerView > ContentView` layout.
+- You must add your content in this `ContentView`.
+- `TitleBar`, `ErrorView` and `SkeletonLoading` are created according to this layout.
+*/
+
+open class Board___VARIABLE_sceneName___ViewController: BoardPartViewController {
+    // MARK: Outlets (grouping view types)
+    
+    
+    // MARK: Variables (in alphabetical order)
+    
+    
+    // MARK: ViewController life cycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+    }
+    
+    // MARK: Function Definitions
+    private func initViews() {
+        //TODO: Add Prepare functions
+    }
+}
+
+  // MARK: Extensions
+
---
+++ CustomMvvmTemplate/CustomMvvmTemplate/BoardFeature.xctemplate/UIViewController/Board___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ IBTECH. All rights reserved.
+//
+
+// MARK: Import (in alphabetical order)
+import MobileCore
+
+class Board___VARIABLE_sceneName___ViewModel: BaseViewModel<___VARIABLE_useCaseName___UseCase, ___VARIABLE_stateName___Model> {
+   
+    // MARK: Variables
+    
+    // Each behaviourRelay variable must be converted to observable variable
+    
+    // MARK: UseCase call methods
+    func getData() {
+       /**
+         Example:
+        */
+        useCase.get___VARIABLE_useCaseName___()
+       
+    }
+}
